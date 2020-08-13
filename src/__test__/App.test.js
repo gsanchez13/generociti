b2d5ca1683c1c3fe1622ffe60690491593464517
gsanchez13 <gisellesanchez@pursuit.org>
2020-08-12 22:40:57 -0400

    sets up files for jest testing

---
+++ src/__test__/App.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import UserProfile from '../Components/UserProfile';
+
+describe("Inital test for app", () => {
+  test("Checks UserProfile component ", () => {
+
+  })
+})
